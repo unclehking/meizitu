63e4bcfe8e2abb6e9e201f3dfa90889639fd04e8
unclehking <513935463@qq.com>
2021-09-14 21:33:46 +0800

    async

--- meizi.js
+++ meizi.js
@@ -11,65 +11,78 @@ const request = require("request");
 const cheerio = require("cheerio");
 const baseUrl = "https://www.mzitu.com/";
 
-function downloadImg(url,dirName){
-	var fName = /\d*\.jpg/.exec(url)[0];
-	console.log(`下载: ${url}`);
-	//解决妹子图防盗链
-	var options = {
-      	uri: url,
-      	headers: {
-        	'If-None-Match': 'W/"5cc2cd8f-2c58"',
-           "Referer": "http://www.mzitu.com/all/",
-           'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 UBrowser/6.1.2107.204 SafarMozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
-      	}
-    };
-	request(options).pipe(fs.createWriteStream(`${dirName}/${fName}`));
+/** 节流 防止被屏蔽IP */
+async function sleep(time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+}
+async function downloadImg(url, dirName) {
+    return new Promise((resolve,reject) => {
+        var fName = /\d*\.jpg/.exec(url)[0];
+        //解决妹子图防盗链
+        var options = {
+            uri: url,
+            headers: {
+                'If-None-Match': 'W/"5cc2cd8f-2c58"',
+                "Referer": "http://www.mzitu.com/all/",
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 UBrowser/6.1.2107.204 SafarMozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
+            }
+        };
+        let writeStream = fs.createWriteStream(`${dirName}/${fName}`, { autoClose: true })
+        request(options).pipe(writeStream);
+        writeStream.on('finish', function () {
+            resolve(`下载: ${url} 完毕！`)
+        })
+        writeStream.on('error', (err) => {
+            reject(err)
+        })
+    })
 }
 
-function download(url){
-	request({
-		uri: url,
-      	headers: {
-        	'If-None-Match': 'W/"5cc2cd8f-2c58"',
-           "Referer": "http://www.mzitu.com/all/",
-           'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 UBrowser/6.1.2107.204 SafarMozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
-      	}
-	}, function(error, response, body){
-		   if(!error && response.statusCode == 200) {
-			   var $ = cheerio.load(body);
-			   var maxNum = $(".pagenavi a").eq(-2).text();
-			   var dirName = "meizi/"+/(\d*)$/.exec(url)[0]+"_"+$(".content h2.main-title").text();
-			   var imgBaseUrl = $(".main-image img").eq(0).attr("src").replace(/[0-9_]*\.jpg$/,"");
-			   //var imgBaseUrl = 'test';
-			   if(!fs.existsSync(dirName)){
-				   fs.mkdir(dirName, 0777, function(err){
-					   if(err){
-						   console.log(err);
-					   }else{
-							dImgs();
-					   }
-				   });
-			   }else{
-				   dImgs();
-			   }
-			   function dImgs(){
-			   		console.log(maxNum);
-				   	for(var i=1;i<=maxNum;i++){
-				   		var num = i > 9 ? i : '0'+i;
-						downloadImg(`${imgBaseUrl}${num}.jpg`,dirName);
-				   	}
-			   }
+function download(url) {
+    request({
+        uri: url,
+        headers: {
+            'If-None-Match': 'W/"5cc2cd8f-2c58"',
+            "Referer": "http://www.mzitu.com/all/",
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 UBrowser/6.1.2107.204 SafarMozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
+        }
+    }, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var $ = cheerio.load(body);
+            var maxNum = $(".pagenavi a").eq(-2).text();
+            var dirName = "" + /(\d*)$/.exec(url)[0] + "_" + $(".content h2.main-title").text();
+            var imgBaseUrl = $(".main-image img").eq(0).attr("src").replace(/[0-9_]*\.jpg$/, "");
+            if (!fs.existsSync(dirName)) {
+                fs.mkdir(dirName, 0777, function (err) {
+                    if (err) {
+                        console.log(err, 22222);
+                    } else {
+                        dImgs();
+                    }
+                });
+            } else {
+                dImgs();
+            }
+            async function dImgs() {
+                console.log(maxNum);
+                for (var i = 1; i <= maxNum; i++) {
+                    var num = i > 9 ? i : '0' + i;
+                    let message = await downloadImg(`${imgBaseUrl}${num}.jpg`, dirName);
+                    console.log(message);
+                    await sleep(2000)
+                }
+            }
 
-		   	}else{
-		   		
-		   	}
-	   });
+        } else {
+
+        }
+    });
 };
 
 const param = process.argv.splice(2);
-param && function(){
-	param.forEach(function(t,i){
-		var url = /^(http)/.test(t) ? t : baseUrl+t;
-		download(url);
-	});
+param && function () {
+    param.forEach(function (t, i) {
+        var url = /^(http)/.test(t) ? t : baseUrl + t;
+        download(url);
+    });
 }();
